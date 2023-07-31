165d48696e09ec0c5fff98e07865a8031461eaa2
Joy-Rec <92474190+Joy-Rec@users.noreply.github.com>
2023-07-31 20:53:39 +0300

    Update ReadMe.m

--- ReadMe.m
+++ ReadMe.m
@@ -1,2 +1,2 @@
-Readme.m
-in this file I'll solve a problem from codewars to improve my problem skills 
+Welcome to my Python Problem Solving repository! Here, you'll find a collection of Python solutions to various programming problems and algorithmic challenges
+This repository is designed to showcase my problem-solving skills using Python and provide a platform for learning and collaboration with the wider coding community.
